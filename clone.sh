b57c9ff970afc51c391638276282a66493dd280c
dyngar <dyngar@dyngar.local>
2015-10-09 21:57:32 -0400

    clone script

---
+++ clone.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+git clone git@github.com:cmotc/schs738c-external_bluetooth.git
+git clone git@github.com:cmotc/schs738c-external_chromium.git
+git clone git@github.com:cmotc/schs738c-external_dnsmasq.git
+git clone git@github.com:cmotc/schs738c-external_e2fsprogs.git
+git clone git@github.com:cmotc/schs738c-external_iproute2.git
+git clone git@github.com:cmotc/schs738c-external_tslib.git
+git clone git@github.com:cmotc/schs738c-external_webkit.git
